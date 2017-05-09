8cbcd77e6f39f18d013748110996e3141a767081
Elian ORIOU <elian.oriou@4sh.fr>
2017-05-10 00:32:58 +0200

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,29 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
+import { VoxxrinApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EventsListComponent } from '../components/events-list/events-list';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    VoxxrinApp,
+    HomePage,
+    EventsListComponent
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(VoxxrinApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    VoxxrinApp,
     HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/components/events-list/events-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  templateUrl: 'events-list.html'
+})
+export class EventsListComponent {
+
+  constructor() {
+  }
+
+}
