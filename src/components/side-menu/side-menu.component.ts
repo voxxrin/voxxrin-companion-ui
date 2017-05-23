7d664ae45034afb8a7ead4f5e7eb89cb52a35b46
Elian ORIOU <elian.oriou@4sh.fr>
2017-05-24 01:00:28 +0200

    add link to events list page

--- src/components/side-menu/side-menu.component.ts
+++ src/components/side-menu/side-menu.component.ts
@@ -1,5 +1,7 @@
+import { NavController } from 'ionic-angular';
 import { Input } from '@angular/core';
 import { Component } from '@angular/core';
+import { EventsPage } from '../../pages/events/events.page';
 
 @Component({
     selector: 'side-menu',
@@ -8,4 +10,12 @@ import { Component } from '@angular/core';
 export class SideMenuComponent {
 
     @Input() content: any;
+
+    constructor(private navCtrl: NavController) {
+
+    }
+
+    goToEventsList(): void{
+        this.navCtrl.push(EventsPage);
+    }
 }
