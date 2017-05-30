9f611c23084e011cc149a7425906c962213ddc2b
Elian ORIOU <elian.oriou@4sh.fr>
2017-05-30 23:00:23 +0200

    improving events list rendering

--- src/components/events-list/events-list.component.ts
+++ src/components/events-list/events-list.component.ts
@@ -1,16 +1,14 @@
 import { Event } from './../../models/event';
-import { Input } from '@angular/core';
+import { Input, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-list',
-  templateUrl: 'events-list.component.html'
+    selector: 'events-list',
+    templateUrl: 'events-list.component.html'
 })
 export class EventsListComponent {
 
-  @Input() events : Event[];
+    @Input() events: Event[];
 
-  constructor() {
-  }
-
+    constructor() { }
 }
--- src/services/event-service.ts
+++ src/services/event-service.ts
@@ -4,16 +4,23 @@ import { environment } from '../app/environment';
 import { Event } from '../models/event';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
+import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Injectable()
 export class EventService {
 
-    constructor(private http: Http) {
-    }
+    constructor(private http: Http) { }
 
     public fetchEvents(): Observable<Event[]> {
         return this.http.get(`${environment.backendApiBaseUrl}/events`)
             .map((response: Response) => response.json())
-            .map((data: any) => data as Event[]);
+            .map((data: any) => data as Event[])
+            .map((events: Event[]) => events.map(event => {
+                event.from = moment(event.from).toDate();
+                event.to = moment(event.to).toDate();
+                return event;
+            }))
+            .map((events: Event[]) => _.orderBy(events, ['from'], ['desc']));
     }
 }
