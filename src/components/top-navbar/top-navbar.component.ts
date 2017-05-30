535b59bb6e2f665b5796b354add7e0a65ea95f98
Alexandre Nedelec <alexandre.nedelec@live.fr>
2017-05-30 22:12:35 +0200

    Improve events-list component

--- src/components/top-navbar/top-navbar.component.ts
+++ src/components/top-navbar/top-navbar.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'top-navbar',
     templateUrl: './top-navbar.component.html'
 })
 export class TopNavbarComponent {
-
+   @Input() title: string;// = "Blabla";
 }
--- src/models/event.ts
+++ src/models/event.ts
@@ -1,5 +1,7 @@
 export interface Event {
     name: string;
+    description: string;
+    location: string;
     from: Date;
     to: Date;
     imageUrl: string;
