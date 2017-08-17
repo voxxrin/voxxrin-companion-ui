09def26e8537ac7a62ba8a7d5b45491dd662d6d5
Alexandre Nedelec <alexandre.nedelec@live.fr>
2017-08-18 00:14:32 +0200

    Add missing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { EventsListComponent } from '../components/events-list/events-list.compo
 import { EventsPage } from '../pages/events/events.page';
 import { FilteredEventsPage } from '../pages/filtered-events/filtered-events.page';
 import { HomePage } from '../pages/home/home.page';
+import { HttpModule } from '@angular/http';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { JWTInterceptor } from '../services/jwt.interceptor.service';
@@ -22,13 +23,13 @@ import { JWTService } from '../services/jwt.service';
 import { LocalStorageService } from '../services/local-storage.service';
 import { LocationService } from '../services/location.service';
 import { MapsComponent } from '../components/maps/maps.component';
+import { PresentationService } from '../services/presentation.service';
 import { PresentationsPage } from '../pages/presentations/presentations.page';
 import { SideMenuComponent } from '../components/side-menu/side-menu.component';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { TopNavbarComponent } from '../components/top-navbar/top-navbar.component';
 import { UtilsService } from '../services/utils.service';
-import { PresentationService } from "../services/presentation.service";
 
 const components = [
     // components
@@ -52,6 +53,7 @@ const components = [
 @NgModule({
     declarations: components,
     imports: [
+        HttpModule,
         HttpClientModule,
         BrowserModule,
         IonicModule.forRoot(AppComponent)
