dcc1d9b3ba96945d0ba63a4d2205790f6747b467
Elian ORIOU <elian.oriou@4sh.fr>
2017-10-17 01:27:13 +0200

    display twitter feed from event page

--- src/pages/tweet-feed/twitter-feed.page.ts
+++ src/pages/tweet-feed/twitter-feed.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedService } from '../../services/feed.service';
-import { NavParams } from 'ionic-angular';
+import { App, NavParams } from 'ionic-angular';
 import { Tweet } from '../../models/tweet.model';
 import { Event } from '../../models/event.model';
+import { EventPage } from '../event/event.page';
 
 @Component({
     templateUrl: './twitter-feed.page.html'
@@ -12,10 +13,14 @@ export class TwitterFeedPage implements OnInit {
     event: Event;
     tweets: Tweet[];
 
-    constructor(private navParams: NavParams, private feedService: FeedService) { }
+    constructor(private app: App, private navParams: NavParams, private feedService: FeedService) { }
 
     ngOnInit(): void {
         this.event = this.navParams.data.event;
         this.feedService.fetchTwitterFeed(this.event).subscribe(tweets => this.tweets = tweets);
+    }
+
+    public goBackToEvent() {
+        this.app.getRootNav().pop();
     }
 }
