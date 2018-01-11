f0ce3e568798c0e0bd93db54d708805103239dcf
tvalli <valli.thomas@gmail.com>
2018-01-11 23:43:01 +0100

    Rename presentation modal to a proper name

---
+++ src/components/attach-content-modal/attach-content-modal.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavParams, ViewController } from 'ionic-angular';
+
+@Component({
+    selector: 'attach-content-modal',
+    templateUrl: 'attach-content-modal.component.html'
+})
+export class AttachContentModalComponent {
+
+    myParam: string;
+
+    constructor(private viewCtrl: ViewController, private params: NavParams) {
+        this.myParam = params.get('userId');
+    }
+
+    dismiss() {
+        this.viewCtrl.dismiss();
+    }
+}
