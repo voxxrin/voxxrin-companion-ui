8ae58413d6af6b3717e0a5e31c6a5afa225a18ba
Elian ORIOU <eoriou@gmail.com>
2018-10-06 21:05:34 +0200

    improving the way the presentations are sorted

--- src/services/time-slot.service.ts
+++ src/services/time-slot.service.ts
@@ -6,17 +6,15 @@ import * as _ from 'lodash';
 @Injectable()
 export class TimeSlotService {
 
-    constructor() { }
-
     public convertPresentationsTimeSlots(presentations: Presentation[]): TimeSlot[] {
         return _.chain(presentations)
-                .groupBy(this.toTimeSlotKey)
-                .map((value:Presentation[], key:string) => new TimeSlot({from: value[0].from, to: value[0].to, presentations: value}))
-                .value();
+            .groupBy(this.toTimeSlotKey)
+            .map((value: Presentation[]) => new TimeSlot({from: value[0].from, to: value[0].to, presentations: value}))
+            .sortBy((slot: TimeSlot) => slot.from)
+            .value();
     }
 
-    private toTimeSlotKey (presentation: Presentation): string {            
+    private toTimeSlotKey(presentation: Presentation): string {
         return presentation.from.format('YYYY-MM-DD-HH-mm') + presentation.to.format('YYYY-MM-DD-HH-mm');
     }
-
 }
