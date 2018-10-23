5ba3e059d22cf703b9de5374edfa119cf4ffeccb
tvalli <valli.thomas@gmail.com>
2018-10-23 21:54:32 +0200

    Add a connectivity service

---
+++ src/services/connectivity.service.ts
@@ -0,0 +1,38 @@
+import { OAuthProvider } from './auth.service';
+import { ToastController, Platform } from 'ionic-angular';
+import { BehaviorSubject } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+import { Network } from '@ionic-native/network';
+
+@Injectable()
+export class ConnectivityService {
+
+    private static isConnected: boolean;
+
+    constructor(private network: Network, private toastCtrl: ToastController, private platform: Platform) {
+        // TODO: see if that can be done elsewhere (ngOnInit ?)
+        this.network.onConnect().subscribe(() => {
+            let toast = this.toastCtrl.create({
+                message: 'Connecté au réseau',
+                duration: 3000,
+                position: 'top'
+            });
+
+        });
+        this.network.onDisconnect().subscribe(() => {
+            let toast = this.toastCtrl.create({
+                message: 'Déconnecté du réseau',
+                duration: 3000,
+                position: 'top'
+            });
+        });
+    }
+
+    public getConnectivity(): boolean {
+        if(this.platform.is('cordova')) {
+            return this.network.type !== this.network.Connection.NONE;
+        }
+        return navigator.onLine;
+
+    }
+}
