76fb7a28c86652218559186c231a0843fb0e4afb
tvalli <valli.thomas@gmail.com>
2018-10-28 21:39:58 +0100

    Prevent the offline/online toast from displaying if the state offline/online hasn't changed

--- src/services/connectivity.service.ts
+++ src/services/connectivity.service.ts
@@ -7,32 +7,41 @@ import { Network } from '@ionic-native/network';
 @Injectable()
 export class ConnectivityService {
 
-    private static isConnected: boolean;
+    private isConnected: boolean = true;
 
     constructor(private network: Network, private toastCtrl: ToastController, private platform: Platform) {
         // TODO: see if that can be done elsewhere (ngOnInit ?)
         this.network.onConnect().subscribe(() => {
-            let toast = this.toastCtrl.create({
-                message: 'Connecté au réseau',
-                duration: 3000,
-                position: 'top'
-            });
+            if (this.isConnected) {
+                let toast = this.toastCtrl.create({
+                    message: 'Connecté au réseau',
+                    duration: 3000,
+                    position: 'top'
+                });
 
-            toast.present();
+                toast.present();
+
+                this.isConnected = true;
+            }
         });
         this.network.onDisconnect().subscribe(() => {
-            let toast = this.toastCtrl.create({
-                message: 'Déconnecté du réseau',
-                duration: 3000,
-                position: 'top'
-            });
-            
-            toast.present();
+            if (!this.isConnected) {
+
+                let toast = this.toastCtrl.create({
+                    message: 'Déconnecté du réseau',
+                    duration: 3000,
+                    position: 'top'
+                });
+
+                toast.present();
+
+                this.isConnected = false;
+            }
         });
     }
 
     public getConnectivity(): boolean {
-        if(this.platform.is('cordova')) {
+        if (this.platform.is('cordova')) {
             return this.network.type !== this.network.Connection.NONE;
         }
         return navigator.onLine;
