c6f62135c6993fd98f17ca237fcc8b712fd443c3
Alexandre Nedelec <alexandre.nedelec@live.fr>
2018-10-28 21:34:53 +0100

    do not try to store presentations in cache if not connected

--- src/components/event/event.component.ts
+++ src/components/event/event.component.ts
@@ -6,6 +6,7 @@ import { DayService } from '../../services/day.service';
 import { Location } from '../../models/location.model';
 import { StoredEventDataService } from '../../services/stored-event-data.service';
 import { PresentationService } from '../../services/presentation.service';
+import { ConnectivityService } from '../../services/connectivity.service';
 
 @Component({
     selector: 'event',
@@ -23,12 +24,14 @@ export class EventComponent implements OnInit {
     constructor(private locationService: LocationService,
                 private dayService: DayService,
                 private presentationService: PresentationService,
-                private storedEventDataService: StoredEventDataService) { }
+                private storedEventDataService: StoredEventDataService,
+                private connectivityService: ConnectivityService) { }
 
     ngOnInit(): void {
         this.locationService.geocode(this.event.location).subscribe(location => this.location = location);
         this.dayService.fetchDays(this.event)
             .do(days => this.days = days)
+            .filter((day, index) => this.connectivityService.getConnectivity())
             .flatMap(days => this.presentationService.fetchAllPresentationFromAnEvent(this.event._id))
             .subscribe(presentations => this.storedEventDataService.storeEventData(this.event, this.days, presentations));
     }
